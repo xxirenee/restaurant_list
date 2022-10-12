f7a0b84eac485c59ab5dddf50ab0a0c96eebd533
xxirenee <charice0709@gmail.com>
2022-10-12 19:57:50 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json').results
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
