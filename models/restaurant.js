f70932627ac2112837e906eaca836a2c2e08658b
xxirenee <charice0709@gmail.com>
2022-10-16 16:53:09 +0800

    modify new.hbs & models

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
   name: { type: String, required: true },
-  name_en: { type: String, required: true },
+  name_en: { type: String },
   category: { type: String, required: true },
   image: { type: String, required: true },
   location: { type: String, required: true },
   phone: { type: String, required: true },
-  google_map: { type: String, required: true },
+  google_map: { type: String },
   rating: { type: Number, required: true },
   description: { type: String, required: true },
   })
