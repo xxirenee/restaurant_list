12d5611e88f5a1d3f8b522290db99215ae3dbe48
xxirenee <charice0709@gmail.com>
2022-10-16 22:21:32 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,16 +11,18 @@ router.get('/', (req, res) => {
 
 //搜尋功能
 router.get('/search', (req, res) => {
-  const keywords = req.query.keywords.trim().toLowerCase()
+
+  const { keywords, sort, order, title } = req.query
   RestaurantList.find({})
+    .sort({ [sort]: order })
     .lean()
     .then(restaurant => {
       const filterRestaurantsData = restaurant.filter(
         data =>
-          data.name.toLowerCase().includes(keywords) ||
-          data.category.includes(keywords)
+          data.name.trim().toLowerCase().includes(keywords.trim().toLowerCase()) ||
+          data.category.includes(keywords.trim())
       )
-      res.render('index', { restaurant: filterRestaurantsData, keywords: keywords })
+      res.render('index', { restaurant: filterRestaurantsData, keywords:keywords, title })
     })
     .catch(err => console.log(err))
 })
