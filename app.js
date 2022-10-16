28174b37956c50b137aad9e11228c514bde384db
xxirenee <charice0709@gmail.com>
2022-10-16 13:08:30 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -3,18 +3,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-
-mongoose.connect(process.env.MONGODB_URI)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
@@ -26,9 +15,6 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-
-
 
 
 app.listen(port, () => {
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,17 +1,7 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../Restaurant')
 const restaurantList = require('../../restaurant.json').results
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
 
