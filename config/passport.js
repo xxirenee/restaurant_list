0944de3b64f1ba5a6e3208cf2a1dae250728e1e1
xxirenee <charice0709@gmail.com>
2022-11-06 21:26:21 +0800

    feat: add isAuthenticated to display different view templates

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-
+const User = require('../models/user')
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
