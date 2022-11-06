a828c36d1241cf7e7d75f9c3f8cf5ec1ee8c4502
xxirenee <charice0709@gmail.com>
2022-11-06 18:44:48 +0800

    feat: add users module, login & register page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
-
+router.use('/users', users)
 module.exports = router
